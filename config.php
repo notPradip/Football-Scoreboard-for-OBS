a5992507cbd0bed3f303907ed8c45acc7b0fe58b
Pradip Adhikari <coderpradip@gmail.com>
2023-01-11 00:43:28 +0545

    New Project Created v1

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php 
+        $server = 'localhost:3306';
+        $username = 'root';
+        $password = '';
+        $database = 'multi';
+        $conn = new mysqli($server, $username, $password, $database) or die("not 
+        connected");
+       
+        ?>

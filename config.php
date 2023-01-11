41d06ce9c85f1ec0cb32949616ca299878f7227b
Pradip Adhikari <coderpradip@gmail.com>
2023-01-12 00:48:43 +0545

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-        $server = 'localhost:3306';
-        $username = 'root';
-        $password = '';
-        $database = 'multi';
-        $conn = new mysqli($server, $username, $password, $database) or die("not 
-        connected");
-       
-        ?>

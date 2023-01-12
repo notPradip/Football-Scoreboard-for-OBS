b63ce5d8562d06eb5edc8d1d9dd0836dad899e43
Pradip Adhikari <coderpradip@gmail.com>
2023-01-12 21:48:40 +0545

    uploaded Config.php file

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php 
+        $server = 'localhost';
+        $username = 'username';
+        $password = 'password';
+        $database = 'database';
+        $conn = new mysqli($server, $username, $password, $database) or die("not 
+        connected");
+       
+        ?>
